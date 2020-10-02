e7d8590e4d26a63b8deb118cc80d4e3be85bd9d7
abhijeet-anshu <abhiva.adi@gmail.com>
2020-10-02 22:38:53 +0530

    xml + java context

--- scratch-5-steps/src/main/java/com/abhijeet/practice/scratch5steps/Scratch5StepsXMLContextApplication.java
+++ scratch-5-steps/src/main/java/com/abhijeet/practice/scratch5steps/Scratch5StepsXMLContextApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.abhijeet.practice.scratch5steps.xml.XmlJdbcConnection;
 import com.abhijeet.practice.scratch5steps.xml.XmlPersonDAO;
 
 @Configuration
@@ -24,6 +23,21 @@ public class Scratch5StepsXMLContextApplication {
 			XmlPersonDAO personDao = applicationContext.getBean(XmlPersonDAO.class);
 			LOGGER.info("{}", personDao);
 			LOGGER.info("{}", personDao.getXmlJdbcConnection());
+			
+			LOGGER.info("Beans Loaded -> {}" , (Object) applicationContext.getBeanDefinitionNames());
+			/*
+			--output
+			Beans Loaded -> [componentDAO, scopedTarget.componentJdbcConnection, 
+			componentJdbcConnection, scratch5StepsBasicApplication, scratch5StepsCdiApplication, 
+			scratch5StepsComponentScanApplication, scratch5StepsScopeApplication, 
+			scratch5StepsXMLContextApplication, scopedTarget.jdbcConnection, jdbcConnection, 
+			personDAO, binarySearchImpl, bubbleSortAlgorithm, quickSortAlgorithm, someCdiBusiness, 
+			someCdiDao, org.springframework.context.annotation.internalConfigurationAnnotationProcessor, 
+			org.springframework.context.annotation.internalAutowiredAnnotationProcessor, 
+			org.springframework.context.annotation.internalCommonAnnotationProcessor, 
+			org.springframework.context.event.internalEventListenerProcessor, 
+			org.springframework.context.event.internalEventListenerFactory, xmlJdbcConnection, xmlPersonDAO]
+			*/
 		}
 		
 		
