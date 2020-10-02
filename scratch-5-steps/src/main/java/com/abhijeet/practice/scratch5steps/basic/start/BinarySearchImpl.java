426a1adfc62d9ce12456014d1521e3c7e68ff0f0
abhijeet-anshu <abhiva.adi@gmail.com>
2020-10-02 18:10:05 +0530

    pre destroy, not guarantied to execute

--- scratch-5-steps/src/main/java/com/abhijeet/practice/scratch5steps/basic/start/BinarySearchImpl.java
+++ scratch-5-steps/src/main/java/com/abhijeet/practice/scratch5steps/basic/start/BinarySearchImpl.java
@@ -1,4 +1,9 @@
 package com.abhijeet.practice.scratch5steps.basic.start;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
@@ -7,6 +12,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype") //by default singleton
 public class BinarySearchImpl {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	@Qualifier("quick")
@@ -24,5 +31,21 @@ public class BinarySearchImpl {
 		//search an Array
 		//Return the result
 		return 3;
+	}
+	
+	@PostConstruct
+	public void pt1() {
+		logger.info("postConstruct, my method name is not important");
+		logger.info("...........getting constructed~~~~~~");
+	}
+	
+	/* does not work unless done the following
+	 * https://stackoverflow.com/questions/16373276/predestroy-method-of-a-spring-singleton-bean-not-called
+	 */
+	@PreDestroy
+	public void anyMethodName() {
+		logger.info("$$$$$$$$$############$$$$$$$$");
+		logger.info("gonna destroy... i can be any method");
+		logger.info("you have to call me");
 	}
 }
